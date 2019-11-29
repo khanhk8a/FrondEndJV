319022ceb6b91920d30392837b536df9dc81981b
khanhk8a <khanhk8a@gmail.com>
2019-11-29 17:38:51 +0700

    them bai menu, may tinh, js

---
+++ Frond1/day1/002/hocjs2.js
@@ -0,0 +1,22 @@
+//prototypes in javascript
+// sử dụng từ khóa new
+// 'new' key word 
+// var today =  new Date();
+// console.log(today);
+
+// function Mouse(color){
+// 	this.weight = 0.2;
+// 	this.color = color;
+// }
+
+// // var mouse1 = new Mouse('White');
+// // var mouse2 = new Mouse('Black');
+
+// // console.log(mouse1);
+// // console.log(mouse2);
+// Mouse.prototype.sleep = function(){
+// 	console.log('sleeping...')
+// };
+// var jerry =  new Mouse('yellow');
+// jerry.sleep;
+// console.log(jerry);
